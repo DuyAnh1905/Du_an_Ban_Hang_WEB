5fb18e1f66f92152994f678f19986e8b07d06fa3
duyanh <danh9499@gmail.com>
2024-12-26 22:13:09 +0700

    first commit

---
+++ src/java/duyanh/dev/admin/DashboardServlet.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package duyanh.dev.admin;
+
+
+
+import duyanh.dev.data.dao.DatabaseDao;
+import duyanh.dev.data.dao.OrderDao;
+import duyanh.dev.data.model.Order;
+import duyanh.dev.util.GetDateTime;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author zacub
+ */
+public class DashboardServlet extends BaseAdminServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        OrderDao orderDao = DatabaseDao.getInstance().getOrderDao();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+
+        int numberOrderToday = orderDao.countOrderByDay(dtf.format(now));
+
+        List<Order> orderList = orderDao.findAll();
+        int numberOrderAll = orderList.size();
+        List<Order> orderPendingList = orderDao.findByStatus("pending");
+
+        int numberOrderPending = orderPendingList.size();
+        int numberOrderFinished = numberOrderAll - numberOrderPending;
+
+        request.setAttribute("numberOrderToday", numberOrderToday);
+        request.setAttribute("numberOrderAll", numberOrderAll);
+        request.setAttribute("numberOrderPending", numberOrderPending);
+        request.setAttribute("orderList", orderList);
+
+//        Chart
+        List<String> dateList = GetDateTime.get7Date();
+        List<Integer> orderByDateList = new ArrayList<>();
+        for (String d : dateList) {
+            int n = orderDao.countOrderByDay(d);
+            orderByDateList.add(n);
+        }
+
+        request.setAttribute("orderByDateList", orderByDateList);
+        request.setAttribute("numberOrderFinished", numberOrderFinished);
+
+        request.setAttribute("dateList", dateList);
+        request.setAttribute("orderByDateList", orderByDateList);
+        request.setAttribute("orderPendingList", orderPendingList);
+        request.getRequestDispatcher("admin/dashboard.jsp").include(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
